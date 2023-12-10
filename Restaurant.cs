050a29ce8cf89ee9737190c0d40ec31fd77a6282
Ivan Strike <ria-2015g@yandex.ru>
2023-12-11 00:11:07 +0300

    add else classes

---
+++ Restaurant.cs
@@ -0,0 +1,19 @@
+﻿namespace oop_project
+{
+    internal class Restaurant
+    {
+        private static int idcounter = 0;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string Description { get; set; }
+        public Restaurant(string name, string adress, string description)
+        {
+            Id = idcounter++;
+            Name = name;
+            Adress = adress;
+            Description = description;
+        }
+
+    }
+}
---
+++ User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_project
+{
+    internal class User
+    {
+        private static int idcouner = 0;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age {  get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public User(string name, string surname, int age, string email ) 
+        { 
+            Id = idcouner++;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Email = email;
+
+        }
+
+    }
+}
