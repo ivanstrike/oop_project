ed4b51e79c58f7cdaa6cc06d899d1039588df655
Ivan Strike <ria-2015g@yandex.ru>
2023-12-13 00:09:19 +0300

    visual, login, signup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,10 +22,71 @@ namespace oop_project
     ///</summary>
     public partial class MainWindow : Window
     {
-        
+        UsersRepository usersRepository = new UsersRepository();
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            string email = EmailTextBox.Text;
+            string password = PasswordBox.Password.ToString();
+            if(usersRepository.LogIn(email, password))
+            {
+                LoginPanel.Visibility = Visibility.Collapsed;
+                ChooseRestaurantPanel.Visibility = Visibility.Visible;
+            }
+            else { MessageBox.Show("Пользователя с такими данными не существует"); }
+        }
+
+        public void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginPanel.Visibility= Visibility.Collapsed;
+            RegistrationPanel.Visibility = Visibility.Visible;
+        }
+        private void Register_Click(object sender, RoutedEventArgs e)
+        {
+            string name = txtName.Text;
+            string surname = txtSurname.Text;
+            string email = txtEmail.Text;
+            string password = ConvertSecureStringToString(PasswordBox.SecurePassword);
+            string adress = txtAddress.Text;
+            if (usersRepository.SignUp(name, surname, email, password, adress))
+            {
+                MessageBox.Show("Успешная регистрация");
+                RegistrationPanel.Visibility = Visibility.Collapsed;
+                LoginPanel.Visibility = Visibility.Visible;
+            }
+            else { MessageBox.Show("Пользователь с такими данными уже существует");}
+
+        }
+        private void BackToLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrationPanel.Visibility= Visibility.Collapsed;
+            LoginPanel.Visibility = Visibility.Visible;
+        }
+        private string ConvertSecureStringToString(SecureString secureString)
+        {
+            IntPtr valuePtr = IntPtr.Zero;
+            try
+            {
+                valuePtr = Marshal.SecureStringToGlobalAllocUnicode(secureString);
+                return Marshal.PtrToStringUni(valuePtr);
+            }
+            finally
+            {
+                Marshal.ZeroFreeGlobalAllocUnicode(valuePtr);
+            }
+        }
+
+        private void SelectRestaurant_Click(object sender, RoutedEventArgs e)
+        {
+            //реализовать выбор ресторана из combobox
+            /*if(Select.SelectedItem == "Вкусно и точка")
+            {
+                ChooseRestaurantPanel.Visibility= Visibility.Collapsed;
+                MenuPanel_1.Visibility= Visibility.Visible;
+            }*/
         }
     }
 }
--- UsersRepository.cs
+++ UsersRepository.cs
@@ -1,36 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace oop_project
 {
     public class UsersRepository
     {
         private List<User> users = new List<User>();
+        private string usersFilePath = "users.txt";
+        
+
         public bool SignUp(string name, string surname, string email, string password, string address)
         {
-            if (UserExists(email))
+            if (CheckUserExists(email))
             {
                 return false; 
             }
             User newUser = new User(name, surname, email, password, address);
             users.Add(newUser);
-            return true;
+            string userData = $"{name},{surname},{email},{password},{address}";
+            try
+            {
+                if (!File.Exists(usersFilePath))
+                {
+                    using (StreamWriter writer = File.CreateText(usersFilePath));
+                    
+                }
+                using (StreamWriter sw = File.AppendText(usersFilePath))
+                {
+                    sw.WriteLine(userData);
+                }
+                return true;
+            }
+            catch (IOException ex)
+            {
+                return false;
+            }
+            
         }
 
-        private bool UserExists(string email)
+        private bool CheckUserExists(string email)
         {
-            foreach (User user1 in users)
+            try
             {
-                if (user1.Email == email) { return true; }
+                string usersFilePath = "users.txt";
+                string[] allLines = File.ReadAllLines(usersFilePath);
+
+                foreach (string line in allLines)
+                {
+                    string[] userData = line.Split(',');
+
+                    if (userData.Length >= 3 && userData[2] == email) 
+                    {
+                        return true; 
+                    }
+                }
+                return false; 
             }
-            return false;
+            catch (IOException ex)
+            {
+                MessageBox.Show("Ошибка при чтении файла: " + ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return true; 
+            }
         }
+
+        public bool CheckUserExistence(string email, string password)
+        {
+            try
+            {
+                string[] allLines = File.ReadAllLines(usersFilePath);
+                foreach (string line in allLines)
+                {
+                    string[] userData = line.Split(',');
+                    if (userData.Length >= 4 && userData[2] == email && userData[3] == password)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch (IOException ex)
+            {
+                return false;
+            }
+        }
+        
         public bool LogIn(string email, string password)
         {
-            if (UserExists(email))
+            if (CheckUserExistence(email,password))
             {
                 return true;
             }
